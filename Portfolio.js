d06b90dd50d8f7e7963e58ab1192cad5bbe5f386
shanmugesh2002 <165014409+shanmugesh2002@users.noreply.github.com>
2024-09-24 13:27:27 +0530

    Create Portfolio.js

---
+++ Portfolio.js
@@ -0,0 +1,6 @@
+function togglebutton(){
+    let menus=document.querySelector('.wrapper');
+    let menuItems=document.querySelector('.wrap-menu-items')
+    menus.classList.toggle('open')
+    menuItems.classList.toggle('open');
+}
